463b9475206a20b0c01a4b5afecdcbf15d8dcccf
Luan K. Alves <luankauan10@gmail.com>
2025-02-11 07:58:37 -0300

    Update empresa_ddl.sql

--- empresa_ddl.sql
+++ empresa_ddl.sql
@@ -14,6 +14,11 @@ CREATE TABLE if not exists FUNCIONARIO(
     Dnr int
     -- primary key (CPF)
 );
+insert funcionario  (PNome,CPF,Sexo,Salario,CPF_Supervisor,Dnr) value ('Alice','99988777707','F', 25000,'98765432168', 4);
+insert funcionario value ('Joice','45345345376','31-07-1972','av Lucas Obes','F', 25000,'33344555587', 5);
+insert funcionario value ('André','98798798733','29-03-1969','rua timbira','M', 25000,'98765432168', 4);
+insert funcionario value ('Jorge','8886555576','10-11-1937','rua do hirto','M', 55000,'33344555587', 1); 
+update funcionario set DataNasc = '16-03-1997' where cpf = 99988777707;
 
 CREATE TABLE DEPARTAMENTO(
 	DNOME varchar(10) NOT NULL,
@@ -26,6 +31,9 @@ CREATE TABLE DEPARTAMENTO(
     ON DELETE CASCADE 
     ON UPDATE CASCADE
 );
+insert DEPARTAMENTO values ('Pesquisa', 5, '33344555587', '22-05-1988' );
+insert DEPARTAMENTO values ('Administração', 4, '98765432168', '01-01-1995' );
+insert DEPARTAMENTO values ('Matriz', 1, '8886555576', '21-06-1981' );
 
 CREATE TABLE if not exists TRABALHA_EM(
 	FCPF char(11) NOT NULL,
@@ -39,6 +47,9 @@ CREATE TABLE if not exists TRABALHA_EM(
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
+insert Trabalha_em values ('12345678966', 1, 32.5);
+insert Trabalha_em values ('12345678966', 2, 7.5); 
+insert Trabalha_em values ('66688444476', 3, 40);
 
 CREATE TABLE if not exists LOCALIZACAO_DEP(
 	DNumero int NOT NULL,
@@ -48,6 +59,11 @@ CREATE TABLE if not exists LOCALIZACAO_DEP(
     ON DELETE CASCADE 
     ON UPDATE CASCADE
 );
+insert LOCALIZACAO_DEP values(1, 'São Paulo');
+insert LOCALIZACAO_DEP values(4, 'Mauá');
+insert LOCALIZACAO_DEP values(5, 'Santo André');
+insert LOCALIZACAO_DEP values(5, 'Itu');
+insert LOCALIZACAO_DEP values(5, 'São Paulo');
 
 CREATE TABLE if not exists PROJETO(
 	ProjNome varchar(15) NOT NULL,
@@ -56,6 +72,12 @@ CREATE TABLE if not exists PROJETO(
     primary key (ProjNumero),
     UNIQUE(ProjNome)
 );
+insert PROJETO values ('Produtox', 1, 'Santo André', 5);
+insert PROJETO values ('Produtoy', 2, 'itu', 5);
+insert PROJETO values ('Produtoz', 3, 'São Paulo', 5);
+insert PROJETO values ('Informatização', 10, 'Mauá', 4);
+insert PROJETO values ('Reorganização', 20, 'São Paulo', 1);
+insert PROJETO values ('NovosBenefícios', 30, 'Mauá', 4);
 
 ALTER TABLE  PROJETO ADD COLUMN DNum int NOT NULL;
 
@@ -66,6 +88,8 @@ on update cascade;
 
 -- ALTER TABLE PROJETO DROP foreign key FK;
 -- ALTER TABLE PROJETO DROP COLUMN DNum;
+
+
 
 
 show tables;
