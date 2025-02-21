06ca0accd3870e3234c057a467c471e245977e6b
Luan K. Alves <luankauan10@gmail.com>
2025-02-21 09:47:23 -0300

    Create ScriptJuncao.sql

---
+++ ScriptJuncao.sql
@@ -0,0 +1,33 @@
+create database JUNCAO;
+
+Use JUNCAO;
+
+create table cargo(
+	CodCargo char(2) NOT NULL primary key,
+    nomeCargo varchar(50) not null,
+    valorCargo decimal(10,2) not null
+);
+
+create table funcionario(
+	matricula integer not null primary key,
+    nomeFuncionario varchar(80) not null,
+    codCargo char(2) default null,
+    foreign key (codCargo) references Cargo(CodCargo)
+);
+
+insert cargo values ('C1', 'Caixa', 800.00);
+insert cargo values ('C2', 'Vendedor', 1200.00);
+insert cargo values ('C3', 'Gerente', 2400.00);
+
+insert funcionario values (100, 'Joao', 'C1');
+insert funcionario values (110, 'Maria', 'C2');
+insert funcionario values (120, 'Carlos', 'C1');
+insert funcionario (Matricula, nomeFuncionario)values(130, 'Tadeu');
+
+select * from Cargo;
+
+select * from funcionario;
+
+select * from cargo C cross join funcionario F;
+
+select nomeFuncionario, nomeCargo from cargo C cross join funcionario F;
