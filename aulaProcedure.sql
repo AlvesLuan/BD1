5243aeeb32d153e19225cfa3759b6a09e7430826
Luan K. Alves <luankauan10@gmail.com>
2025-05-14 11:29:21 -0300

    Create aulaProcedure.sql

---
+++ aulaProcedure.sql
@@ -0,0 +1,50 @@
+create database procedimento_funcao;
+use procedimento_funcao;
+create table tabela1 (
+cod int primary key,
+nome varchar(45)
+);
+
+insert into tabela1 value (1,'Ian');
+insert into tabela1 value (2,'Danilo');
+insert into tabela1 value (3,'Pedro');
+insert into tabela1 value (4,'João');
+
+DELIMITER $
+CREATE PROCEDURE teste()
+BEGIN
+
+	Select 'hello';
+    
+END $$
+DELIMITER ;
+
+CALL teste();
+
+SELECT * from tabela1;
+DELIMITER $$
+CREATE PROCEDURE teste2(IN quantidade INT)
+BEGIN
+	SELECT * FROM tabela1
+    LIMIT quantidade;
+END $$
+
+CALL teste2(2);
+CALL teste2(3);
+
+DELIMITER $$
+
+CREATE PROCEDURE teste3 (OUT quantidade INT)
+BEGIN
+	SELECT COUNT(*) INTO quantidade FROM tabela1;
+END $$
+
+DELIMITER ;
+CALL teste3(@quantidade);
+select @quantidade;
+
+
+
+
+SET @ti=1, @t2=2, @t3:=4;
+select @t1, @t2, @t3, @t4 := @t1+@t2+@t3;
