cde5140b1a4574d8fff3d1f072336b714abe0689
Luan K. Alves <luankauan10@gmail.com>
2025-05-15 09:56:38 -0300

    Update aulaProcedure.sql

--- aulaProcedure.sql
+++ aulaProcedure.sql
@@ -62,3 +62,87 @@ SET @valor = 5;
 CALL teste4(@valor);
 SELECT @valor;
 
+
+DELIMITER $$
+CREATE FUNCTION quadrado (numero INT) RETURNS int
+BEGIN
+	SET numero = numero * numero;
+    RETURN numero;
+END$$
+DELIMITER ;
+
+select quadrado(5);
+-- drop function quadrado;
+
+
+SET GLOBAL log_bin_trust_function_creators = 1;
+DELIMITER $$
+CREATE FUNCTION quadrado (numero INT) RETURNS int
+BEGIN
+	SET numero = numero * numero;
+    RETURN numero;
+END$$
+DELIMITER ;
+
+select quadrado(5);
+
+
+
+DELIMITER $$
+Create procedure teste5()
+Begin
+	declare x int;
+    declare y float;
+    set y = 1.1;
+    select count(*) into x from tabela1;
+    select x,y;
+END $$
+DELIMITER ;
+-- drop procedure teste5
+
+call teste5();
+
+
+DELIMITER $
+CREATE PROCEDURE teste6 (in a int)
+BEGIN
+	declare x float;
+    if (a >= 0) then set x = 10;
+    else set x = 20;
+    end if;
+    select x;
+END $$
+DELIMITER ;
+
+call teste6(1);
+call teste6(0);
+call teste6(-1);
+
+
+DELIMITER $
+CREATE PROCEDURE teste7(in a int, in b int)
+BEGIN
+IF a > b THEN
+	SELECT 'VALOR A MAIOR';
+ELSEIF A < B THEN
+	SELECT 'VALOR A MENOR';
+ELSE
+	SELECT 'MESMO VALOR';
+END IF;
+END $$
+DELIMITER ;
+
+
+DELIMITER $$
+CREATE procedure teste8()
+begin
+	declare str varchar(255) default ' ';
+    declare x int DEFAULT 0;
+    SET X = 1;
+    WHILE x <= 5 DO
+		SET str = CONCAT(str, x, ' , ');
+        SET x = x + 1;
+	END WHILE;
+    SELECT STR;
+END $$
+DELIMITER ;
